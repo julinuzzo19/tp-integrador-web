3a0ec00809fb9afd3e8aa4587be22802d6b635d4
julinuzzo19 <julinuzzo19@gmail.com>
2021-10-04 23:22:43 -0300

    agredados articulos dinamicos en idnex

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,48 @@
-// import {} from './constants.js'
+import {URL_API_CHARACTERS, URL_API_SERIES, URL_API} from './constants.js';
 
+$(document).ready(() => {
+  let URL = URL_API_CHARACTERS + '&limit=8';
+});
 
+$.get({
+  url: `${URL_API_CHARACTERS}&limit=8`,
+  success: (response) => {
+    let articlesFounded = response.data.results;
 
-// $(document).ready(() => {
-//   console.log('object');
-// });
-// $.get({
-//   url: `https://gateway.marvel.com:443/v1/public/characters?apikey=009e32091293e7d1531b865c1241db7f`,
-//   success: (data) => {
-//     console.log(data);
-//   }
-// });
+    for (const article of articlesFounded) {
+      $('#div-personajes').append(`
+        <article class="article-personajes">
+        <img
+          src="${article.thumbnail.path}/portrait_medium.jpg"
+          class="img-article"
+        />
+        <div>
+          <h4 class="desc-article">${article.name}</h4>   
+        </div>
+      </article>`);
+    }
+  }
+});
+
+$.get({
+  url: `${URL_API_SERIES}&limit=8`,
+  success: (response) => {
+    let articlesFounded = response.data.results;
+    for (const article of articlesFounded) {
+      $('#div-series').append(`
+    <article class="article-series">
+             <img
+               src="${article.thumbnail.path}/portrait_incredible.jpg"
+               class="img-article"
+             />
+             <div>
+               <p>
+                ${article.title}
+                 <br />
+                 <small>JUL 2, 2019</small>
+               </p>
+             </div>
+           </article>`);
+    }
+  }
+});
